7312d90fd10c261b083632e3f1f9db818851e04d
Liubov <98466098+Liubov36@users.noreply.github.com>
2022-10-16 21:36:56 +0200

    add files

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Item = styled.li`
+  margin-bottom: 15px;
+`;
+
+export const Text = styled.p`
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+export const Number = styled.span`
+  font-size: 20px;
+  font-weight: 500;
+`;
